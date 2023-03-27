3cb77d9e84146b58f619dacb02a73a7c819aa51a
Andreas Kokkalis <akokkalis@gmail.com>
2023-03-27 21:56:25 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_excel('trasn_cost.xlsx')
+
+
+st.set_page_config(page_title='my page', page_icon=':car:',)
+st.title('Odoiporika Diathtwn 2022-2023')
+st.write('omorfa')
+# st.table(df)
+# st.dataframe(df)
+st.sidebar.header('Please Filter Here')
+city = st.sidebar.selectbox("Select City:", options=df["No"].unique(),)
+field = st.sidebar.selectbox("Select Stadium:", list(df)[1:],)
+st.write(field)
+
+st.info(f' From {city} to {field}')
+df_select = df.query("No == @city")[['No', field]]
+st.dataframe(df_select)
+st.dataframe(df_select[-1][-1])
